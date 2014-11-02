2f8a3eb1225daef2f9c962199dba243537147e30
Javier Soto <sotoseattle@gmail.com>
2014-11-02 08:42:07 -0800

    modify spec helper require paths for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 # factor based
-require_relative '../lib/primo/factors/random_var'     # Class
-require_relative '../lib/primo/factors/factor'        # Class
-require_relative '../lib/primo/factors/factor_array'   # Class
+require './lib/primo/factors/random_var'     # Class
+require './lib/primo/factors/factor'        # Class
+require './lib/primo/factors/factor_array'   # Class
 # graph based
-require_relative '../lib/primo/graphs/node'           # Class
-require_relative '../lib/primo/graphs/messenger'      # Module
-require_relative '../lib/primo/graphs/graph'          # Module
-require_relative '../lib/primo/graphs/tree'           # Module
-require_relative '../lib/primo/graphs/induced_markov'  # Class
-require_relative '../lib/primo/graphs/clique_tree'     # Class
+require './lib/primo/graphs/node'           # Class
+require './lib/primo/graphs/messenger'      # Module
+require './lib/primo/graphs/graph'          # Module
+require './lib/primo/graphs/tree'           # Module
+require './lib/primo/graphs/induced_markov'  # Class
+require './lib/primo/graphs/clique_tree'     # Class
